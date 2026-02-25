2876382cace722f75cfdac940198fd2bfea42292
Jahongir <ojahongir2197@gmail.com>
2026-02-25 15:51:31 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+# 1
+def salomlashuv():
+    print("Salom, dunyo!")
+
+salomlashuv()
+
+
+# 2
+def raqam_korsatish():
+    print(100)
+
+raqam_korsatish()
+
+
+# 3
+def motivatsiya():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+motivatsiya()
+
+
+# 4
+def takror_matn():
+    print("Python")
+    print("Python")
+    print("Python")
+
+takror_matn()
+
+
+# 5
+def oddiy_hisob():
+    print(5 + 7)
+
+oddiy_hisob()
+
+
+# 6
+def vaqt_xabari():
+    print("Hozir tong!")
+
+vaqt_xabari()
+
+
+# 7
+def fikr():
+    print("O'rganish - kelajak!")
+
+fikr()
+
+
+# 8
+def boshlanish():
+    print("Dastur ishga tushdi!")
+
+boshlanish()
+
+
+# 9
+def tasodifiy_salom():
+    print("Bugun yaxshi kun!")
+
+tasodifiy_salom()
+
+
+# 10
+def oddiy_chiqarish():
+    print("Xush kelibsiz!")
+
+oddiy_chiqarish()
